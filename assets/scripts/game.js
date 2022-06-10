0539b21159615234e3fdc2e6e977efa9da7d6cef
paulo-sinkawa <101948725+paulo-sinkawa@users.noreply.github.com>
2022-06-10 20:30:45 -0300

    vinculo de html com js e css + imagens

---
+++ assets/scripts/game.js
@@ -0,0 +1,11 @@
+class Game {
+  constructor(gameBoard) {
+    // Sera onde iremos criar toda a plataforma do jogo.
+    this.gameBoard = gameBoard;
+    this.vitoria = "./assets/images/vitoria.jpg";
+    this.derrota = "./assets/images/derrota.jpg";
+    this.tesoura = "./assets/images/tesoura.jpg";
+    this.papel = "./assets/images/papel.jpg";
+    this.pedra = "./assets/images/pedra.jpg";
+  }
+}
--- index.js
+++
@@ -1,3 +0,0 @@
-let h1 = document.getElementById("h1");
-
-h1.innerText = "Teste de link html com js";

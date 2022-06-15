ea0566f8b9c0cfeca513000dce3534fc45560c77
paulo-sinkawa <101948725+paulo-sinkawa@users.noreply.github.com>
2022-06-15 16:03:50 -0300

    Finalizado1

--- assets/scripts/classes.js
+++ assets/scripts/classes.js
@@ -1,47 +1,41 @@
 class Game {
   constructor(gameBoard) {
-    // Sera onde iremos criar toda a plataforma do jogo.
-    this.gameBoard = gameBoard; //
-    this.inicio = "../assets/images/inicio.jpg";
-    // this.opcao = [
-    //   "../assets/images/tesoura.jpg",
-    //   "../assets/images/papel.jpg",
-    //   "../assets/images/pedra.jpg",
-    // ]; // Esta variavel foi criada para que lá na frente, poderemos embaralhar as opcoes e ramdomizar a opcao do computador.
-    this.tesoura = "../assets/images/tesoura.jpg";
-    this.papel = "../assets/images/papel.jpg";
-    this.pedra = "../assets/images/pedra.jpg";
+    this.gameBoard = gameBoard;
     this.jogadaFeita = false;
     this.compJajogou = false;
     this.opcaoJogador = "";
     this.opcaoComputador = 0;
+    this.existeVencedor = false;
   }
 
   jogadaPedra() {
     if (this.jogadaFeita) {
-      alert("Voce ja fez sua jogada !");
+      alert("Voce ja fez sua jogada ! Aperte F5 para atualizar a pagina.");
       return;
     }
     this.opcaoJogador = "pedra";
     this.jogadaFeita = true;
+    return;
   }
 
   jogadaPapel() {
     if (this.jogadaFeita) {
-      alert("Voce ja fez sua jogada !");
+      alert("Voce ja fez sua jogada ! Aperte F5 para atualizar a pagina.");
       return;
     }
     this.opcaoJogador = "papel";
     this.jogadaFeita = true;
+    return;
   }
 
   jogadaTesoura() {
     if (this.jogadaFeita) {
-      alert("Voce ja fez sua jogada !");
+      alert("Voce ja fez sua jogada ! Aperte F5 para atualizar a pagina.");
       return;
     }
     this.opcaoJogador = "tesoura";
     this.jogadaFeita = true;
+    return;
   }
 
   jogadaComputador() {
@@ -63,77 +57,63 @@ class Game {
   }
 
   verificaVencedor() {
-    if (this.opcaoJogador === "pedra" && this.opcaoComputador === "tesoura") {
-      alert(
-        "O computador escolheu tesoura, voce venceu ! Aperte F5 para jogar novamente"
-      );
-      return;
-    } else if (
-      this.opcaoJogador === "tesoura" &&
-      this.opcaoComputador === "papel"
-    ) {
-      alert(
-        "O computador escolheu papel, voce venceu ! Aperte F5 para jogar novamente"
-      );
-      return;
-    } else if (
-      this.opcaoJogador === "papel" &&
-      this.opcaoComputador === "pedra"
-    ) {
-      alert(
-        "O computador escolheu pedra, voce venceu ! Aperte F5 para jogar novamente"
-      );
-      return;
-    } else if (
-      this.opcaoComputador === "pedra" &&
-      this.opcaoJogador === "tesoura"
-    ) {
-      alert(
-        "O computador escolheu pedra, voce perdeu ! Aperte F5 para jogar novamente"
-      );
-      return;
-    } else if (
-      this.opcaoComputador === "tesoura" &&
-      this.opcaoJogador === "papel"
-    ) {
-      alert(
-        "O computador escolheu tesoura, voce perdeu ! Aperte F5 para jogar novamente"
-      );
-      return;
-    } else if (
-      this.opcaoComputador === "papel" &&
-      this.opcaoJogador === "pedra"
-    ) {
-      alert(
-        "O computador escolheu papel, voce perdeu ! Aperte F5 para jogar novamente"
-      );
-      return;
-    } else {
-      alert("Empate !");
-      return;
+    if (!this.existeVencedor) {
+      if (this.opcaoJogador === "pedra" && this.opcaoComputador === "tesoura") {
+        alert(
+          "O computador escolheu tesoura, voce venceu ! Aperte F5 para jogar novamente"
+        );
+        this.existeVencedor = true;
+        return;
+      } else if (
+        this.opcaoJogador === "tesoura" &&
+        this.opcaoComputador === "papel"
+      ) {
+        alert(
+          "O computador escolheu papel, voce venceu ! Aperte F5 para jogar novamente"
+        );
+        this.existeVencedor = true;
+        return;
+      } else if (
+        this.opcaoJogador === "papel" &&
+        this.opcaoComputador === "pedra"
+      ) {
+        alert(
+          "O computador escolheu pedra, voce venceu ! Aperte F5 para jogar novamente"
+        );
+        this.existeVencedor = true;
+        return;
+      } else if (
+        this.opcaoComputador === "pedra" &&
+        this.opcaoJogador === "tesoura"
+      ) {
+        alert(
+          "O computador escolheu pedra, voce perdeu ! Aperte F5 para jogar novamente"
+        );
+        this.existeVencedor = true;
+        return;
+      } else if (
+        this.opcaoComputador === "tesoura" &&
+        this.opcaoJogador === "papel"
+      ) {
+        alert(
+          "O computador escolheu tesoura, voce perdeu ! Aperte F5 para jogar novamente"
+        );
+        this.existeVencedor = true;
+        return;
+      } else if (
+        this.opcaoComputador === "papel" &&
+        this.opcaoJogador === "pedra"
+      ) {
+        alert(
+          "O computador escolheu papel, voce perdeu ! Aperte F5 para jogar novamente"
+        );
+        this.existeVencedor = true;
+        return;
+      } else {
+        alert("Empate !");
+        this.existeVencedor = true;
+        return;
+      }
     }
   }
 }
-
-// verificaVencedor() {
-//   const primeiraOpcao =
-//     this.opcaoJogador === "pedra" && this.opcaoComputador === "tesoura";
-//   const segundaOpcao =
-//     this.opcaoJogador === "tesoura" && this.opcaoComputador === "papel";
-//   const terceiraOpcao =
-//     this.opcaoJogador === "papel" && this.opcaoComputador === "pedra";
-//   const quartaOpcao =
-//     this.opcaoComputador === "pedra" && this.opcaoJogador === "tesoura";
-//   const quintaOpcao =
-//     this.opcaoComputador === "tesoura" && this.opcaoJogador === "papel";
-//   const sextaOpcao =
-//     this.opcaoComputador === "papel" && this.opcaoJogador === "pedra";
-//   const setimaOpcao = this.opcaoJogador === this.opcaoComputador;
-//   if (primeiraOpcao || segundaOpcao || terceiraOpcao) {
-//     alert("O jogador venceu o computador !");
-//   } else if (quartaOpcao || quintaOpcao || sextaOpcao) {
-//     alert("O computador venceu o jogador !");
-//   } else {
-//     alert("Empate !");
-//   }
-// }
--- assets/scripts/inicio.js
+++ assets/scripts/inicio.js
@@ -1,6 +1,3 @@
-// let h1 = document.getElementById("h1");
-// h1.innerText = "Ta funcionando inicio.html com inicio.js"; // Somente para teste.
-
 const gameBoard = document.getElementById("game-board");
 const escolherPedra = document.getElementById("pedra");
 const escolherPapel = document.getElementById("papel");
@@ -25,27 +22,3 @@ escolherTesoura.addEventListener("click", () => {
   game.jogadaComputador();
   game.verificaVencedor();
 });
-
-//mostrarJogadaComputador() - Evento que vai separar por parte a jogada do jogador, depois ele clicar em
-//um botao para fazer a jogada do computador e mostrar a jogada e imprimir o resultado.
-
-// iniciarBtn.addEventListener("click", () => {
-//   game.iniciaOJogo();
-// });
-//
-// const jogarPedra = document.getElementById("pedra");
-// const jogarPapel = document.getElementById("papel");
-// const jogarTesoura = document.getElementById("tesoura");
-// jogarPedra.addEventListener("click", () => {
-//   game.jogadaPedra();
-// });
-
-// jogarPapel.addEventListener("click", () => {
-//   game.jogadaPapel();
-// });
-
-// jogarTesoura.addEventListener("click", () => {
-//   game.jogadaTesoura();
-// });
-
-// const opcaoJogador = ""; // Verificar como será lancada a opcao do jogador e verificar qual foi a opcao.
